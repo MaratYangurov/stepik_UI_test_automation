b17d2cf9d60bc5f667e4ef80d41b84fd2a2066fb
MaratYangurov <marat.yangurov@gmail.com>
2026-09-18 18:12:00 +0500

    Коммит_5: закончил 5.5 Описываем методы работы с браузером

---
+++ src/ui/browser/browser.py
@@ -0,0 +1,48 @@
+from playwright.sync_api import Cookie
+from playwright.sync_api import Page
+
+
+class Browser:
+    """Класс для взаимодействия с методами браузера, а также вкладками и iframe"""
+    def __init__(self, page: Page):
+        self.page = page
+
+
+    def do_to_url(self, url: str):
+        """Переходит по указанному URL"""
+        return self.page.goto(url)
+
+    def reload_page(self):
+        """Перезаписывает страницу"""
+        return self.page.reload()
+
+    def get_cookies(self):
+        """Получает куки страницы"""
+        return self.page.context.cookies()
+
+    def add_cookie(self, cookies: Cookie):
+        """Передаем список куков в хранилище браузера"""
+        return self.page.context.add_cookies(cookies)
+
+    def close_tab(self, number: int):
+        """Закрывает страницу с указанным порядковым номером"""
+
+        all_tabs = self.page.context.pages
+        all_tabs[number].close()
+
+    def switch_tab(self, number: int):
+        """Переходит на страницу с указанным номером и закрывает предыдущие вкладки"""
+
+        all_tabs = self.page.context.pages
+        new_tab = all_tabs[number]
+        new_tab.bring_to_front()
+        new_tab.wait_for_load_state()
+        return new_tab
+
+    def switch_to_iframe_and_click(self, iframe_locator: str, locator_for_click: str):
+        """Переходит на iframe и кликает по локатору в iframe"""
+        frame = self.page.frame_locator(iframe_locator)
+        frame.locator(locator_for_click).click()
+
+    def alert_accept(self):
+        """Принимает диалоговое окно"""
